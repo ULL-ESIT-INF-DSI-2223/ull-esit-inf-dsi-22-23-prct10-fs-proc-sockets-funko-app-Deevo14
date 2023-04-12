3f21c7571a9d828a9680f043107fd15f777fca40
Deevo14 <alu0101052168@ull.edu.es>
2023-04-12 01:30:56 +0100

    Ini

--- src/mod/client.ts
+++
@@ -1,8 +0,0 @@
-import { FilterMapReduce } from "./operations.js";
-import { FilterMapAddReduce } from "./add.js";
-
-function clientCode(fmr: FilterMapReduce):number {
-    return fmr.run((num: number) => num > 5, (num:number) => num + 2);
-}
-  
-clientCode(new FilterMapAddReduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
